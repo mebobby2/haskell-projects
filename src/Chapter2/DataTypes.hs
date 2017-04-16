4e7a13098074b829baf55b9ef06f436d84fb35ab
Bobby Lei <bobl53@gmail.com>
2017-04-16 16:14:20 +0800

    timemachine data type

--- src/Chapter2/DataTypes.hs
+++ src/Chapter2/DataTypes.hs
@@ -10,3 +10,8 @@ data Person = Person String String Gender
 
 data Gender = Male | Female | Unknown
             deriving Show
+
+data Manufacturer = Manufacturer String deriving Show
+data Model = Model Integer deriving Show
+data TimeMachine = TimeMachine String Manufacturer Model Bool Double
+                 deriving Show
