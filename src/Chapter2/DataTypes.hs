9ee69c69e33dfc6182cd0d1ab076b3af20bbe66b
Bobby Lei <bobl53@gmail.com>
2017-04-16 16:03:12 +0800

    broke datatypes into its own module

---
+++ src/Chapter2/DataTypes.hs
@@ -0,0 +1,12 @@
+module Chapter2.DataTypes where
+
+data Client = GovOrg String
+            | Company String Integer Person String
+            | Individual Person Bool
+            deriving Show
+
+data Person = Person String String Gender
+            deriving Show
+
+data Gender = Male | Female | Unknown
+            deriving Show
