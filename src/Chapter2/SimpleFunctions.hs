ad647c4f24a93ee6529b20a96a4b8f2ed827969e
Bobby Lei <bobl53@gmail.com>
2017-04-16 15:04:41 +0800

    adding +++ and reverse2 functions

--- src/Chapter2/SimpleFunctions.hs
+++ src/Chapter2/SimpleFunctions.hs
@@ -1,2 +1,14 @@
 module Chapter2.SimpleFunctions where
-  firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+(+++) :: [a] -> [a] -> [a]
+lst1 +++ lst2 = if null lst1 {- check emptiness -}
+                then lst2 -- base case
+                else (head lst1) : (tail lst1 +++ lst2)
+
+reverse2 :: [a] -> [a]
+reverse2 list = if null list
+                then []
+                else reverse2 (tail list) +++ [head list]
