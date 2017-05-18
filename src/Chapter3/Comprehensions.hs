36b6cb520f33670f51fbb6514dff76926d3bcb4b
Bobby Lei <bobl53@gmail.com>
2017-05-18 20:41:51 +0800

    finished list comprehensions

--- src/Chapter3/Comprehensions.hs
+++ src/Chapter3/Comprehensions.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE TransformListComp, RecordWildCards, ParallelListComp #-}
+
 module Chapter3.Comprehensions where
 
 import Chapter3.ParamPoly
+import Data.Char
+import GHC.Exts
 
 
 --doubleOdds :: [Integer] -> [Integer]
@@ -12,4 +16,37 @@ doubleOdds list = [ 2 * x | x <- list, odd x]
 govClientNames :: [Client a] -> [String]
 govClientNames listOfClients = [ clientName x | x@(GovOrg _ _) <- listOfClients ]
 
---multiplicationsOfFour :: [(Integer,Integer,Integer)]
+multiplicationsOfFour :: [(Integer,Integer,Integer)]
+multiplicationsOfFour = [(x,y,x*y) | x <- [1 .. 4], y <- [1 .. 10]]
+
+dominos :: [(Integer, Integer)]
+dominos = [(x,y) | x <- [0 .. 6], y <- [x .. 6]]
+
+
+upperString = [ toUpper c | s <- ["This", "is", "a", "list"], c <- ' ':s]
+
+vectorNorms = [ sqrt v | (x,y) <- [(1,2),(3,8)], let v = x*x+y*y]
+
+dominosWithGuard = [(x,y) | x <- [1 .. 6], y <- [1 .. 6], x <= y]
+
+multipleReverse = [x*y | x <- [-1,1,-2], y <- [1,2,3], then reverse]
+
+sortList = [x*y | x <- [-1,1,-2], y <- [1,2,3], then sortWith by x]
+
+--sortGroupList = [(the p, m) | x <- [-1,1,-2]
+--                            , y <- [1,2,3]
+--                            , let m = x*y
+--                            , let p = m > 0,
+--                            , then group by p using groupWith ]
+
+companyAnalytics :: [Client a] -> [(String, [(Person, String)])]
+companyAnalytics clients = [ (the clientName, zip person duty)
+                           | client@(Company { .. }) <- clients
+                           , then sortWith by duty
+                           , then group by clientName using groupWith
+                           , then sortWith by length client
+                           ]
+
+traditionalNesting = [ x*y | x <- [1,2,3], y <- [1,2,3]]
+zipping = [ x*y | x <- [1,2,3] | y <- [1,2,3]]
+
