dc7c2f384e719dd6430c31e924f010c0a00eec39
Bobby Lei <bobl53@gmail.com>
2017-05-23 11:09:44 +0800

    playing with infinite lists

--- README.md
+++ README.md
@@ -436,5 +436,5 @@ https://github.com/apress/beg-haskell
 
 # Upto
 
-Page 112
-Chapter 5
+Page 114
+There are even more tricks with infinite lists. Let’s look now at an interesting way to define Fibonacci numbers
---
+++ src/Chapter5/Annotations.hs
@@ -0,0 +1,29 @@
+module Chapter5.Annotations where
+
+data TimeMachine = TM { manufactuer :: String, year :: Integer } deriving (Eq, Show)
+
+timeMachinesFrom :: String -> Integer -> [TimeMachine]
+timeMachinesFrom mf y = TM mf y : timeMachinesFrom mf (y+1)
+
+timelyIncMachines :: [TimeMachine]
+timelyIncMachines = timeMachinesFrom "Timely Inc." 100
+
+--take 3 timelyIncMachines
+
+--import Data.List
+--find (\(TM { year = y }) -> y > 2018) timelyIncMachines
+
+
+
+--(\list -> zip [1 .. length list] list) "abcd"
+--The above is inefficient because you have to traverse the list twice: once to get its length, and once again to zip both.
+--We will optimize it with a infinite list.
+
+allNumbers :: [Integer]
+allNumbers = allNumbersFrom 1
+
+allNumbersFrom :: Integer -> [Integer]
+allNumbersFrom n = n : allNumbersFrom (n+1)
+
+--zip allNumbers "abcd"
+--zip [1 .. ] "abcd"
