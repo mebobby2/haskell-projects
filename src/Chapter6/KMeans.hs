1354171d4061b3c39ee090b221a03dd9d1be65df
Bobby Lei <bobl53@gmail.com>
2017-06-02 21:49:50 +0800

    syntax errors

--- src/Chapter6/KMeans.hs
+++ src/Chapter6/KMeans.hs
@@ -5,7 +5,7 @@ import Chapter6.Vector
 import Data.List
 import qualified Data.Map as M
 
-clusterAssignmentPhase :: (Vector v, Vectorizable e v) => [v] -> [e] => M.Map v [e]
+clusterAssignmentPhase :: (Vector v, Vectorizable e v) => [v] -> [e] -> M.Map v [e]
 clusterAssignmentPhase centroids points =
   let initialMap = M.fromList $ zip centroids (repeat [])
    in foldr (\p m -> let chosenCentroid = minimumBy (\x y -> compare (distance x $ toVector p)
@@ -18,7 +18,7 @@ newCentroidPhase :: (Vector v, Vectorizable e v) => M.Map v [e] -> [(v,v)]
 newCentroidPhase = M.toList . fmap (centroid . map toVector)
 
 shouldStop :: (Vector v) => [(v,v)] -> Double -> Bool
-shouldStop centroids threshold = foldr (\(x y) s -> s + distance x y) 0.0 centroids < threshold
+shouldStop centroids threshold = foldr (\(x,y) s -> s + distance x y) 0.0 centroids < threshold
 
 kMeans :: (Vector v, Vectorizable e v) => (Int -> [e] -> [v]) -- initialization function
                                        -> Int                 -- number of centroids
