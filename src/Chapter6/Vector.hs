94b1d0f85728767faac93acbf0d2ed77d9433f4f
Bobby Lei <bobl53@gmail.com>
2017-06-02 22:13:43 +0800

    assigning the ghci extensions to where they are used

--- src/Chapter6/Vector.hs
+++ src/Chapter6/Vector.hs
@@ -8,6 +8,7 @@ class (Default v, Ord v) => Vector v where
   distance :: v -> v -> Double
   centroid :: [v] -> v
 
+               --FlexibleInstances
 instance Vector (Double, Double) where
   distance (a,b) (c,d) = sqrt $ (c-a)*(c-a) + (d-b)*(d-b)
   centroid lst = let (u,v) = foldr (\(a,b) (c,d) -> (a+c,b+d)) (0.0,0.0) lst
@@ -18,5 +19,6 @@ instance Vector (Double, Double) where
 class Vector v => Vectorizable e v where
   toVector :: e -> v
 
+                             --MultiParamTypeClasses
 instance Vectorizable (Double,Double) (Double,Double) where
   toVector = id
