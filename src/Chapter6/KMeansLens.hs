b2b2f18325dd2ecf694e2bf55171c2444633a5eb
Bobby Lei <bobl53@gmail.com>
2017-06-05 11:42:28 +0800

    upto

--- src/Chapter6/KMeansLens.hs
+++ src/Chapter6/KMeansLens.hs
@@ -18,6 +18,15 @@ makeLenses ''KMeansState
 initializeState :: (Int -> [e] -> [v]) -> Int -> [e] -> Double -> KMeansState e v
 initializeState i n pts t = KMeansState (i n pts) pts (1.0/0.0) t 0
 
+clusterAssignments :: (Vector v, Vectorizable e v) => KMeansState e v -> M.Map v [e]
+clusterAssignments state =
+  let initialMap = M.fromList $ zip (state^.centroids) (repeat [])
+   in foldr (\p m -> let chosenCentroid = minimumBy (\x y -> compare (distance x $ toVector p)
+                                                                     (distance y $ toVector p))
+                                                     (state^.centroids)
+                      in M.adjust (p:) chosenCentroid m)
+            initialMap (state^.points)
+
 kMeans :: (Vector v, Vectorizable e v) => (Int -> [e] -> [v]) -> Int -> [e] -> Double -> [v]
 kMeans i n pts t = view centroids $ kMeans' (initializeState i n pts t)
 
@@ -25,7 +34,7 @@ kMeans' :: (Vector v, Vectorizable e v) => KMeansState e v -> KMeansState e v
 kMeans' state =
   let assignments = clusterAssignments state
       state1 = state & centroids.traversed
-                     %~ (\c -> centroid % fmap toVector $ M.findWithDefault [] c assignments)
+                     %~ (\c -> centroid $ fmap toVector $ M.findWithDefault [] c assignments)
       state2 = state1 & err .~ sum (zipWith distance (state^.centroids) (state1^.centroids))
       state3 = state2 & steps +~ 1
    in if state3^.err < state3^.threshold then state3 else kMeans' state3
