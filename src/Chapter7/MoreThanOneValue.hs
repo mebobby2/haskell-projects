d7548c4fdc380d29f757ed9c1b905cb93584e637
Bobby Lei <bobl53@gmail.com>
2017-06-15 10:53:48 +0800

    note

--- src/Chapter7/MoreThanOneValue.hs
+++ src/Chapter7/MoreThanOneValue.hs
@@ -8,6 +8,17 @@ brokenThreeJumps y = do firstJ  <- brokenJump y
                         secondJ <- brokenJump firstJ
                         brokenJump secondJ
 
+--:{
+--do x <- [1,2,3]
+--   y <- [7,8,9]
+--   return $ x * y
+--:}                        
+
+--Notice how in the example above, we need to use return. The list nomad will automatically concat the results and 
+--return them. However, it can only cancat list types. x * y returns a number, not a list, there we must use return
+--to convert the number into a list. However, inside the function brokenThreeJumps we do not need to use return 
+--because brokenJump already returns an list.
+
 brokenJumps :: Integer -> Integer -> [Integer]
 brokenJumps n year = brokenJumps' n [year]
   where brokenJumps' 0 years = years
